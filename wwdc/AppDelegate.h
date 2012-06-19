8c93db0912ed56d47902101ae639dce695389d5b
Chris Lowe <hashtagstudios@gmail.com>
2012-06-18 22:48:19 -0500

    Initial Commit

---
+++ wwdc/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  wwdc
+//
+//  Created by Chris Lowe on 6/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
