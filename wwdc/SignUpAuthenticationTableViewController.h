27b4113198ff2b34f2447482ef688b4edf656bb3
Chris Lowe <hashtagstudios@gmail.com>
2012-06-19 01:24:51 -0500

    Added new subclasses

--- wwdc/SignUpAuthenticationTableViewController.h
+++ wwdc/SignUpAuthenticationTableViewController.h
@@ -3,7 +3,7 @@
 //  wwdc
 //
 //  Created by Chris Lowe on 6/19/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2012 Hashtag Studios. All rights reserved.
 //
 
 #import "AuthenticationTableViewController.h"
